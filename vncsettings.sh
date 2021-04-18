f48ae50dababe40ed94f61bf8e62eec619c0a314
Kiyotaka Nemoto <kiyotaka@nemotos.net>
2021-04-19 02:53:22 +0900

    change permission

--- vncsettings.sh
+++ vncsettings.sh
@@ -2,12 +2,12 @@
 # A script to set vncserver and novnc
 # Usage: $0 <resolution>
 # resolution should be something like 1920x1080
+# K. Nemoto 19 Apr 2021
 
 if [ $# -lt 1 ]; then
   RESOLUTION=1280x800
 else
   RESOLUTION=$1
-
 fi
 
 USER=brain vncserver :1 -geometry ${RESOLUTION} -depth 24
